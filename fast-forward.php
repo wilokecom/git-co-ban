dcc7b1c11941bb3fa243a7a2e88db233b39b5b69
Wiloke <contact.wilcity@gmail.com>
2021-02-09 11:40:14 +0700

    Fixed fast forward
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- fast-forward.php
+++ fast-forward.php
@@ -3,10 +3,6 @@ echo "First";
 echo "Second";
 echo "Third";
 echo "MM";
-<<<<<<< HEAD
-echo "dad1";
-=======
 echo "dad1";
 echo "First";
 echo "dad1";
->>>>>>> bdba8d236e8cb2730014d53c1d43f59acee944ea
